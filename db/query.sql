93be8c0de0a06f8adc26ef28c7f35b431246abf9
prieta <sofiaboubion@gmail.com>
2024-01-31 21:59:53 -0800

    Updated readMe file

--- db/query.sql
+++ db/query.sql
@@ -5,3 +5,15 @@ SELECT * FROM company_db.department;
 SELECT * FROM company_db.role;
 
 SELECT * FROM company_db.employee;
+
+SELECT * FROM employee;
+
+INSERT INTO department(name) VALUES ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("data scientist", 90000, 1);
+
+SELECT title FROM role;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Shoua", "Chang", 7, 1);
